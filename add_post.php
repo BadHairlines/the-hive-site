338c0e5a4e607ffabd23a60fd9a66c37058c0e03
BadHairlines <105704515+BadHairlines@users.noreply.github.com>
2026-01-18 07:21:20 -0500

    Update add_post.php

--- add_post.php
+++ add_post.php
@@ -1,20 +1,19 @@
 <?php
-$type = $_POST['type'];
+$type = $_POST['type']; // news or events
 $allowed = ['news','events'];
+
 if (!in_array($type, $allowed)) die("Invalid type");
+
 $file = $type . '.json';
-$title = htmlspecialchars($_POST['title']);
-$date = $_POST['date'];
-$content = htmlspecialchars($_POST['content']);
-if (!DateTime::createFromFormat('Y-m-d', $date)) {
-    die("Invalid date format, must be YYYY-MM-DD");
-}
-$data = json_decode(file_get_contents($file), true) ?? [];
+
+$data = json_decode(file_get_contents($file), true);
+
 $data[] = [
-    "title" => $title,
-    "date" => $date,
-    $type === 'news' ? "content" : "description" => $content
+    "title" => $_POST['title'],
+    "date" => $_POST['date'],
+    // Use 'content' for news, 'description' for events
+    $type === 'news' ? "content" : "description" => $_POST['content']
 ];
-file_put_contents($file, json_encode($data, JSON_PRETTY_PRINT), LOCK_EX);
+
+file_put_contents($file, json_encode($data, JSON_PRETTY_PRINT));
 header("Location: admin.html");
-exit;
